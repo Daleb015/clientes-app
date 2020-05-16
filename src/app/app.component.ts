b3fa70885ead1b880cb3c6b24901348b2a144e23
Daleb015 <daleb015@gmail.com>
2020-05-16 00:11:05 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a angular';
+
+  curso:  string = 'Curso Spring 5 Con Angular 7';
+  profesor: string = 'Andrés Guzman';
 }

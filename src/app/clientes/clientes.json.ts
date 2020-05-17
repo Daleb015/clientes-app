dc0285c7d4c08569c8a4686e6e069faa55871389
Daleb015 <daleb015@gmail.com>
2020-05-17 11:43:00 -0500

    Se separa el objetos clientes del componente, se implementa clienteservice

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,8 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Jorge', apellido: 'Caso', email: 'emailJorge@hotmail.com', createAt: '2018-12-07'},
+  {id: 2, nombre: 'Flora', apellido: 'Bueno', email: 'emailFlora@gmail.com', createAt: '2018-12-07'},
+  {id: 3, nombre: 'David', apellido: 'Caso', email: 'emailDavid@hotmail.com', createAt: '2018-12-06'},
+  {id: 4, nombre: 'Reme', apellido: 'Carrascal', email: 'emailReme@gmail.com', createAt: '2018-12-05'},
+  {id: 5, nombre: 'Ángel', apellido: 'Caso', email: 'emailAngel@gmail.com', createAt: '2018-12-04'}
+];

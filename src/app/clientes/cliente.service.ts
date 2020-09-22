1be4c2c9e5adceca3ba5034099992989d5ae4c06
Daleb015 <daleb015@gmail.com>
2020-09-21 22:46:48 -0500

    Se habilita gestion al estado 400 en clientesservi

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -6,7 +6,7 @@ import { of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import swal from 'sweetalert2';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ClienteService {
@@ -22,63 +22,64 @@ export class ClienteService {
   }
 
   create(cliente: Cliente): Observable<Cliente> {
-    return this.http.post(this.urlEndPoint, cliente, {
-      headers: this.httpHeaders,
-    }).pipe(
-      map( (response: any) => response.cliente as Cliente),
-      catchError(e => {
-
-/*         if (e.status==400) {
-          return throwError(e)
-        } */
-
-        console.error(e.error.mensaje)
-        swal(e.error.mensaje, e.error.error, 'error')
-        return throwError(e)
-      })
-    );
-  }
-
-  getCliente(id): Observable<Cliente> {
     return this.http
-      .get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
-        catchError(e => {
-          this.router.navigate(['/clientes']);
+      .post(this.urlEndPoint, cliente, {
+        headers: this.httpHeaders,
+      })
+      .pipe(
+        map((response: any) => response.cliente as Cliente),
+        catchError((e) => {
+          if (e.status == 400) {
+            return throwError(e);
+          }
+
           console.error(e.error.mensaje);
-          swal(e.error.mensaje, e.error.error, 'error')
+          swal(e.error.mensaje, e.error.error, 'error');
           return throwError(e);
         })
       );
   }
 
-  update(cliente: Cliente): Observable<any> {
-    return this.http.put<any>(
-      `${this.urlEndPoint}/${cliente.id}`,
-      cliente,
-      { headers: this.httpHeaders }
-    ).pipe(
-      catchError(e => {
-
-        /*         if (e.status==400) {
-          return throwError(e)
-        } */
-
-        console.error(e.error.mensaje)
-        swal(e.error.mensaje, e.error.error, 'error')
-        return throwError(e)
+  getCliente(id): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError((e) => {
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje);
+        swal(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
       })
     );
   }
 
-  delete(id: string): Observable<Cliente> {
-    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {
-      headers: this.httpHeaders,
-    }).pipe(
-      catchError(e => {
-        console.error(e.error.mensaje)
-        swal(e.error.mensaje, e.error.error, 'error')
-        return throwError(e)
+  update(cliente: Cliente): Observable<any> {
+    return this.http
+      .put<any>(`${this.urlEndPoint}/${cliente.id}`, cliente, {
+        headers: this.httpHeaders,
       })
-    );
+      .pipe(
+        catchError((e) => {
+          if (e.status == 400) {
+            return throwError(e);
+          }
+
+          console.error(e.error.mensaje);
+          swal(e.error.mensaje, e.error.error, 'error');
+          return throwError(e);
+        })
+      );
+  }
+
+  delete(id: string): Observable<Cliente> {
+    return this.http
+      .delete<Cliente>(`${this.urlEndPoint}/${id}`, {
+        headers: this.httpHeaders,
+      })
+      .pipe(
+        catchError((e) => {
+          console.error(e.error.mensaje);
+          swal(e.error.mensaje, e.error.error, 'error');
+          return throwError(e);
+        })
+      );
   }
 }

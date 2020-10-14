67601540550e30c7c73103565ea0c0ce8299ba9b
Daleb015 <daleb015@gmail.com>
2020-10-14 07:51:33 -0500

    se agrega clase usuario, se mapea usuario en vista y genera metodo login

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo: string = 'Sign In!';
+  usuario: Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario= new Usuario;
+  }
 
   ngOnInit(): void {
+  }
+
+  login(): void{
+    console.log(this.usuario);
+    if (this.usuario.username==null||this.usuario.password==null) {
+      swal('Error de Login','Password o nombre vacios!','error');
+      return;
+    }
+
   }
 
 }
---
+++ src/app/usuarios/usuario.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+  id: string;
+  username: string;
+  password: string;
+  nombre: string;
+  apellido: string;
+  email: string;
+  roles: string [] = []
+}

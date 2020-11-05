ebe1ed8d4ee3cdb30570f6305095fb6c1ccfcdcb
Daleb015 <daleb015@gmail.com>
2020-11-04 21:06:51 -0500

    se agrego role guard para verificar que los accesos a url se protegan de acuerdo al rol del usuario logueado

--- src/app/usuarios/guards/role.guard.ts
+++ src/app/usuarios/guards/role.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+import swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router){
+
+  }
+
   canActivate(
-    route: ActivatedRouteSnapshot,
+    next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+      if (!this.authService.isAuthenticathed()) {
+        this.router.navigate(['/login'])
+        return false;
+      }
+
+      let role = next.data['role'] as string;
+      console.log(role);
+      if (this.authService.hasRole(role)) {
+        return true;
+      }
+      swal('Accesso Denegado',`${this.authService.usuario.username} No tiene acceso a esta función`,'warning')
+      this.router.navigate(['/clientes'])
+      return true;
   }
   
 }

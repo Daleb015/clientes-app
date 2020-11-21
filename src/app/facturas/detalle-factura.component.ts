b2dfd74046ee8d1e25fab2cedcf51cc6522af291
Daleb015 <daleb015@gmail.com>
2020-11-21 10:25:54 -0500

    se implementa vista factura en el front

--- src/app/facturas/detalle-factura.component.ts
+++ src/app/facturas/detalle-factura.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Factura } from './models/factura';
+import { FacturaService } from './services/factura.service';
+
 
 @Component({
   selector: 'app-detalle-factura',
@@ -6,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleFacturaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private facturaService: FacturaService, private activatedRoute: ActivatedRoute) { }
+
+  factura: Factura;
+  titulo: string = 'Factura';
+
+  id: string;
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let id = params.get('id');
+      this.facturaService.getFactura(id).subscribe(factura => {
+        this.factura = factura;
+      });
+    })
   }
 
 }

180d3707aa723a8b8ef3f183281efc230d3c1856
Daleb015 <daleb015@gmail.com>
2020-11-21 14:34:07 -0500

    se crea factura component y la vista de ese componente se enlaza al clientes component html

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ClienteService } from '../clientes/cliente.service';
+import { Factura } from './models/factura';
 
 @Component({
   selector: 'app-facturas',
-  templateUrl: './facturas.component.html'
+  templateUrl: './facturas.component.html',
 })
 export class FacturasComponent implements OnInit {
+  titulo: string = 'nueva factura';
+  factura: Factura = new Factura();
 
-  constructor() { }
+  constructor(
+    private clienteService: ClienteService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      let clienteId = params.get('clienteId');
+      this.clienteService
+        .getCliente(clienteId)
+        .subscribe((cliente) => (this.factura.cliente = cliente));
+    });
   }
-
 }
